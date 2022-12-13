7cb27e93df540fdfb442ee3e23c58398c5e76ca2
Unto Karjalainen <unto@untok.fi>
2022-12-13 03:26:34 +0200

    Initial commit

---
+++ hCaptcha/includes/Hooks/ResourceLoaderHooks.php
@@ -0,0 +1,22 @@
+<?php
+declare( strict_types=1 );
+namespace MediaWiki\Extension\ConfirmEdit\hCaptcha\Hooks;
+use Config;
+use MediaWiki\ResourceLoader as RL;
+class ResourceLoaderHooks {
+	/**
+	 * Passes config variables to ext.confirmEdit.hCaptcha.visualEditor ResourceLoader module.
+	 * @param RL\Context $context
+	 * @param Config $config
+	 * @return array
+	 */
+	public static function getHCaptchaResourceLoaderConfig(
+		$context,
+		Config $config
+	) {
+		return [
+			'hCaptchaSiteKey' => $config->get( 'HCaptchaSiteKey' ),
+			'hCaptchaScriptURL' => 'https://js.hcaptcha.com/1/api.js',
+		];
+	}
+}
